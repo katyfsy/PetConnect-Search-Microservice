b23c2ca518b9ea970df288bcb781029822f71fc1
katyfsy <katyfsy@gmail.com>
2022-08-25 17:04:16 -0400

    Initial commit

---
+++ src/main/java/com/vmware/CapstoneSearch/CapstoneSearchApplication.java
@@ -0,0 +1,13 @@
+package com.vmware.CapstoneSearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CapstoneSearchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CapstoneSearchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vmware/CapstoneSearch/CapstoneSearchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vmware.CapstoneSearch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CapstoneSearchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
