e7ba8160d08bdf623ea1fda0ad99dc91453a4956
Marilene Soares da Costa <lene626@gmail.com>
2022-08-26 13:51:10 -0600

    adding setup to main -not working

--- src/main/java/com/vmware/CapstoneSearch/CapstoneSearchApplication.java
+++ src/main/java/com/vmware/CapstoneSearch/CapstoneSearchApplication.java
@@ -1,13 +1,34 @@
 package com.vmware.CapstoneSearch;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @SpringBootApplication
 public class CapstoneSearchApplication {
+	@Autowired
+	PetsRepository petsRepository;
+	List<Pet> pets;
+
+	public static void setup(){
+		this.pets = new ArrayList<>();
+		for (int i = 0; i < 5; i++) {
+			this.pets.add(new Pet("dog", "lucky"+i));
+		}
+		for (int i = 0; i < 5; i++) {
+			this.pets.add(new Pet("cat", "jingles"+i));
+		}
+		petsRepository.saveAll(this.pets);
+	}
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(CapstoneSearchApplication.class, args);
+		setup();
 	}
+
 
 }
