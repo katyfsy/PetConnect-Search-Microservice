e0187dbb4561f197c79c9dc5290834dd6903bce0
TofuStore <humormy@gmail.com>
2022-08-30 10:17:44 -0700

    added local postgresql support

--- src/main/java/com/vmware/CapstoneSearch/PetsRepository.java
+++ src/main/java/com/vmware/CapstoneSearch/PetsRepository.java
@@ -8,4 +8,5 @@ import java.util.List;
 @Repository
 public interface PetsRepository extends JpaRepository<Pet, Long> {
     List<Pet> findByZipIn(List<String> zip);
+    List<Pet> findByZipInAndTypeContains(List<String> zip, String type);
 }
