59971a2bebcf6860696c1ceec5a890eb41da1285
Meredith White <meredith.white91@gmail.com>
2022-08-31 15:34:49 +0000

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -1,57 +1,16 @@
--- SET check_function_bodies = false;
+/* run command: psql <username> - h 127.0.0.1 - d petsProfiles - f schema.sql */
 
 -- /* Reset */
--- DROP DATABASE test IF EXISTS;
--- CREATE test;
+DROP DATABASE pets IF EXISTS;
+CREATE petsProfiles;
 
 
 -- ==== THIS IS JUST AN EXAMPLE / CHANGE SCHEMA TO REFLECT CURRENT PROJECT ===
-/* Table 'questions' */
-CREATE TABLE questions(
+/* Table 'pets' */
+CREATE TABLE pets(
   id serial NOT NULL,
-  product_id integer NOT NULL,
-  body text NOT NULL,
-  date_written bigint NOT NULL,
-  asker_name varchar(255),
-  asker_email text,
-  reported bool,
-  helpful integer,
+  name varchar(255) NOT NULL,
+  type varchar(255) NOT NULL,
   PRIMARY KEY(id)
 );
 
-/* Table 'answers' */
-CREATE TABLE answers(
-  id serial NOT NULL,
-  body text,
-  date_written bigint NOT NULL,
-  answerer_name varchar(255),
-  answerer_email text,
-  reported bool,
-  helpful integer,
-  questions_id integer NOT NULL,
-  PRIMARY KEY(id)
-);
-
-/* Table 'answer_photos' */
-CREATE TABLE answer_photos(
-  id serial NOT NULL,
-  url text,
-  answers_id integer NOT NULL,
-  questions_id integer NOT NULL,
-  PRIMARY KEY(id)
-);
-
-/* Relation 'questions_answers' */
-ALTER TABLE answers
-  ADD CONSTRAINT questions_answers
-    FOREIGN KEY (questions_id) REFERENCES questions (id);
-
-/* Relation 'answers_answer_photos' */
-ALTER TABLE answer_photos
-  ADD CONSTRAINT answers_answer_photos
-    FOREIGN KEY (answers_id) REFERENCES answers (id);
-
-/* Relation 'questions_answer_photos' */
-ALTER TABLE answer_photos
-  ADD CONSTRAINT questions_answer_photos
-    FOREIGN KEY (questions_id) REFERENCES questions (id);
