b031407f93265947d4327749e988ef5cb70cb56b
katyfsy <katyfsy@gmail.com>
2022-09-01 16:58:05 -0400

    update type of age to varchar, update fake data

--- schema.sql
+++ schema.sql
@@ -14,32 +14,32 @@ CREATE TABLE pets(
   name varchar(255) NOT NULL,
   type varchar(255) NOT NULL,
   zip varchar(255) NOT NULL,
-  age int NOT NULL,
+  age varchar(255) NOT NULL,
   gender varchar(255) NOT NULL,
   breed varchar(255) NOT NULL,
   PRIMARY KEY(id)
 );
 
 INSERT INTO pets(id, name, type, zip, age, gender, breed)
-VALUES (DEFAULT, 'Mr. Whiskers', 'cat', '98745', 1, 'female', 'ragdoll');
+VALUES (DEFAULT, 'Mr. Whiskers', 'cat', '98745', 'young', 'female', 'ragdoll');
 
 INSERT INTO pets(id, name, type, zip, age, gender, breed)
-VALUES (DEFAULT, 'Beans', 'cat', '95745', 1, 'male', 'bengal');
+VALUES (DEFAULT, 'Beans', 'cat', '95745', 'adult', 'male', 'bengal');
 
 INSERT INTO pets(id, name, type, zip, age, gender, breed)
-VALUES (DEFAULT, 'Baguera', 'cat', '88745', 5, 'female', 'british shorthair');
+VALUES (DEFAULT, 'Baguera', 'cat', '88745', 'senior', 'female', 'british shorthair');
 
 INSERT INTO pets(id, name, type, zip, age, gender, breed)
-VALUES (DEFAULT, 'Bitey McNibble', 'cat', '78745', 7, 'male', 'devon rex');
+VALUES (DEFAULT, 'Bitey McNibble', 'cat', '78745', 'young', 'male', 'devon rex');
 
 INSERT INTO pets(id, name, type, zip, age, gender, breed)
-VALUES (DEFAULT, 'Sam', 'dog', '98745', 2, 'male', 'bulldog');
+VALUES (DEFAULT, 'Sam', 'dog', '98745', 'adult', 'male', 'bulldog');
 
 INSERT INTO pets(id, name, type, zip, age, gender, breed)
-VALUES (DEFAULT, 'Bark Twain', 'dog', '68745', 3, 'female', 'golden retriever');
+VALUES (DEFAULT, 'Bark Twain', 'dog', '68745', 'senior', 'female', 'golden retriever');
 
 INSERT INTO pets(id, name, type, zip, age, gender, breed)
-VALUES (DEFAULT, 'Buster', 'dog', '58745', 1, 'female', 'husky');
+VALUES (DEFAULT, 'Buster', 'dog', '58745', 'young', 'female', 'husky');
 
 INSERT INTO pets(id, name, type, zip, age, gender, breed)
-VALUES (DEFAULT, 'Chewy', 'dog', '48745', 4, 'male', 'corgi');
+VALUES (DEFAULT, 'Chewy', 'dog', '48745', 'adult', 'male', 'corgi');
