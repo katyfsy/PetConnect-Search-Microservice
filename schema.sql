8e025ee7d793049d8dabf2c7e3ede6a42804db5a
katyfsy <katyfsy@gmail.com>
2022-09-01 11:28:07 -0400

    updated schema with age, gender, and breed

--- schema.sql
+++ schema.sql
@@ -14,30 +14,32 @@ CREATE TABLE pets(
   name varchar(255) NOT NULL,
   type varchar(255) NOT NULL,
   zip varchar(255) NOT NULL,
+  age int NOT NULL,
+  gender varchar(255) NOT NULL,
+  breed varchar(255) NOT NULL,
   PRIMARY KEY(id)
 );
 
-INSERT INTO pets(id, name, type, zip)
-VALUES (DEFAULT, 'Mr. Whiskers', 'cat', '98745');
+INSERT INTO pets(id, name, type, zip, age, gender, breed)
+VALUES (DEFAULT, 'Mr. Whiskers', 'cat', '98745', 1, 'female', 'ragdoll');
 
-INSERT INTO pets(id, name, type, zip)
-VALUES (DEFAULT, 'Beans', 'cat', '95745');
+INSERT INTO pets(id, name, type, zip, age, gender, breed)
+VALUES (DEFAULT, 'Beans', 'cat', '95745', 1, 'male', 'bengal');
 
-INSERT INTO pets(id, name, type, zip)
-VALUES (DEFAULT, 'Baguera', 'cat', '88745');
+INSERT INTO pets(id, name, type, zip, age, gender, breed)
+VALUES (DEFAULT, 'Baguera', 'cat', '88745', 5, 'female', 'british shorthair');
 
-INSERT INTO pets(id, name, type, zip)
-VALUES (DEFAULT, 'Bitey McNibble', 'cat', '78745');
+INSERT INTO pets(id, name, type, zip, age, gender, breed)
+VALUES (DEFAULT, 'Bitey McNibble', 'cat', '78745', 7, 'male', 'devon rex');
 
+INSERT INTO pets(id, name, type, zip, age, gender, breed)
+VALUES (DEFAULT, 'Sam', 'dog', '98745', 2, 'male', 'bulldog');
 
-INSERT INTO pets(id, name, type, zip)
-VALUES (DEFAULT, 'Sam', 'dog', '98745');
+INSERT INTO pets(id, name, type, zip, age, gender, breed)
+VALUES (DEFAULT, 'Bark Twain', 'dog', '68745', 3, 'female', 'golden retriever');
 
-INSERT INTO pets(id, name, type, zip)
-VALUES (DEFAULT, 'Bark Twain', 'dog', '68745');
+INSERT INTO pets(id, name, type, zip, age, gender, breed)
+VALUES (DEFAULT, 'Buster', 'dog', '58745', 1, 'female', 'husky');
 
-INSERT INTO pets(id, name, type, zip)
-VALUES (DEFAULT, 'Buster', 'dog', '58745');
-
-INSERT INTO pets(id, name, type, zip)
-VALUES (DEFAULT, 'Chewy', 'dog', '48745');
+INSERT INTO pets(id, name, type, zip, age, gender, breed)
+VALUES (DEFAULT, 'Chewy', 'dog', '48745', 4, 'male', 'corgi');
