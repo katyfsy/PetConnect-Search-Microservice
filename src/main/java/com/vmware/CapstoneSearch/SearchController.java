8ea82219bdc6ad5b05fe0913c68616c3f62f4ce5
Katy Feng <katyfsy@gmail.com>
2022-09-23 20:24:50 +0000

    change crossorigin to *, update docker-compose file

--- src/main/java/com/vmware/CapstoneSearch/SearchController.java
+++ src/main/java/com/vmware/CapstoneSearch/SearchController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 //allowing frontend in from port 3000
-@CrossOrigin(origins="http://localhost:3000")
+@CrossOrigin(origins="*")
 @RestController
 
 public class SearchController {
